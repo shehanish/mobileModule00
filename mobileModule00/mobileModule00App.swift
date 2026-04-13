a0f291316cdc2c0a7816615d3d14b1985113f72e
shehanish <shehani1207@gmail.com>
2026-04-13 12:31:05 +0200

    set up swift-project

---
+++ mobileModule00/mobileModule00App.swift
@@ -0,0 +1,17 @@
+//
+//  mobileModule00App.swift
+//  mobileModule00
+//
+//  Created by Shehani Hansika on 13.04.26.
+//
+
+import SwiftUI
+
+@main
+struct mobileModule00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
